d342a564a26c7f5598ea123245854c4092874219
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2023-05-17 17:59:27 -0300

    add annotation feature

--- MarvelUI/Core/Home/View/UberMapViewRepresentable.swift
+++ MarvelUI/Core/Home/View/UberMapViewRepresentable.swift
@@ -25,6 +25,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
             print("DEBUG: Selected coordinate in map view \(coordinate)")
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
         }
     }
     
@@ -36,12 +37,18 @@ struct UberMapViewRepresentable: UIViewRepresentable {
 extension UberMapViewRepresentable {
     
     class MapCoordinator: NSObject, MKMapViewDelegate {
+        
+        // MARK: - Properties
         let parent: UberMapViewRepresentable
+        
+        // MARK: - Lifecycle
         
         init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
         }
+        
+        // MARK: - MKMapViewDelegate
         
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(
@@ -53,6 +60,16 @@ extension UberMapViewRepresentable {
             )
             
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        // MARK: - Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            self.parent.mapView.addAnnotation(anno)
+            self.parent.mapView.selectAnnotation(anno, animated: true)
         }
     }
 }
